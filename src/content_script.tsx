98beff5c96bcc248584f2de9abc8c73bb7cedba4
paulbuehler <paulbuehlerdev@gmail.com>
2024-02-10 18:09:30 +0100

    feat: add fetch collation

--- src/content_script.tsx
+++ src/content_script.tsx
@@ -1,5 +1,7 @@
 import { fetchReach } from "./content/fetchReach";
+import { fetchCollation } from "./content/fetchCollation";
 
 console.info("Initialize Meta Ad Helper!");
 
 fetchReach(277715382000909).then((reach) => console.log(reach));
+fetchCollation("222002310979685").then((collation) => console.log(collation));
