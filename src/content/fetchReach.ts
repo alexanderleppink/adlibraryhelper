d407d77d4f9351a0c83403ced416b4aa22fdecd1
paulbuehler <paulbuehlerdev@gmail.com>
2024-02-11 13:30:33 +0100

    chore: improve error logging

--- src/content/fetchReach.ts
+++ src/content/fetchReach.ts
@@ -2,22 +2,38 @@ import { createUrlEncoded } from "../utils/utils";
 import { createFetchOptions } from "./facebookRequest";
 
 export async function fetchReach(id: number) {
-  return fetch("https://www.facebook.com/api/graphql/", createFetchOptions({
-    headers: {
-      "x-asbd-id": "129477",
-      "x-fb-friendly-name": "AdLibraryAdDetailsV2Query",
-    },
-    body: createUrlEncoded({
-      doc_id: "6635716889819821",
-      variables: JSON.stringify({
-        adArchiveID: id,
-        country: "ALL",
-        source: null,
-        isAdNonPolitical: true,
-        isAdNotAAAEligible: false
+  return fetch(
+    "https://www.facebook.com/api/graphql/",
+    createFetchOptions({
+      headers: {
+        "x-asbd-id": "129477",
+        "x-fb-friendly-name": "AdLibraryAdDetailsV2Query",
+      },
+      body: createUrlEncoded({
+        doc_id: "6635716889819821",
+        variables: JSON.stringify({
+          adArchiveID: id,
+          country: "ALL",
+          source: null,
+          isAdNonPolitical: true,
+          isAdNotAAAEligible: false,
+        }),
       }),
-    }),
-  })).then(response => response.json()
-    .then(data => data?.data?.ad_library_main?.ad_details?.aaa_info?.eu_total_reach)
-    .catch(async (error) => console.log('error', error, "responseData", await response.text())));
+    })
+  )
+    .then((response) =>
+      response.text().then((text) => {
+        try {
+          const data = JSON.parse(text);
+          return data?.data?.ad_library_main?.ad_details?.aaa_info
+            ?.eu_total_reach;
+        } catch (error) {
+          console.error("Error parsing response", error);
+          console.error("Response", text);
+          console.error("Status", response.status);
+          return null;
+        }
+      })
+    )
+    .catch((error) => console.error("Request failed", error));
 }
