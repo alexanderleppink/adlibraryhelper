9c7ce0949548ed6ca05200091ad0240877e4d049
paulbuehler <paulbuehlerdev@gmail.com>
2024-02-12 01:00:48 +0100

    chore: remove load test

--- src/content_script.tsx
+++ src/content_script.tsx
@@ -1,13 +1,8 @@
 import { fetchCollation } from "./content/fetchCollation";
 import { watchAds } from "./content/watchAds";
 import { mutateAd } from "./content/mutateAd";
-import { fetchReachLoad } from "./content/fetchReachLoad";
 
 console.info("Initialize Meta Ad Helper!");
-
-fetchReachLoad(100, 1).then(() => {
-  console.info("Reach load done!");
-});
 
 fetchCollation("222002310979685");
 
